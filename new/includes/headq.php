5ab5f8331195877f5a8fd2869429c3644ddb81b2
Janhavi Talhar <60478538+T-janhavi11@users.noreply.github.com>
2021-05-04 15:45:21 +0530

    Add files via upload

---
+++ new/includes/headq.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+ <html>
+ <head>
+ <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300&display=swap" rel="stylesheet">
+<a href= "login_form.php">
+    <img src="user-login-icon-14.png" alt="tiger" width="100" height="100"></a>
+ <style>
+
+  body {
+    margin:0 12;
+    height: 100%;
+    width: 100%;
+
+    } ul {
+   list-style-type: none;
+    margin: 10px 4%;
+    padding: 9px;
+     overflow: hidden;
+      background-color: #333;
+     
+      top: 0;
+      width: 90%;
+
+       }
+
+
+       li {
+        float: left;
+         }
+         li a {
+          display: block;
+           color: white;
+            text-align: center;
+             padding: 14px 80px;
+              text-decoration: none;
+               }
+                li a:hover:not(.active) {
+                  background-color:teal;
+                   }
+
+                   .active {
+                    background-color: teal;
+                     }
+
+                      </style>
+                      </head>
+                      <body background="pictures/home.jpg" fixed background-position: center>
+
+
+                       <ul>
+                       <li>
+
+                       <a href="home.php"> Home</a></li> <li>
+                      </li> <li>
+
+                      </li><li>
+                    <a href="artworks.php">Artworks</a> </li> <li>
+                       <a href="display_artist.php">Artist </a> </li> <li>
+
+
+                       <a href="guidelines.php">Guidelines</a> </li> <li>
+                       <a href="about_us.php">About us</a> </li> <li>
+
+                     </li> <li> </li></ul>
+
+
+                        </div>
+
+                        </body>
+                         </html>
+
+
+
+
+
